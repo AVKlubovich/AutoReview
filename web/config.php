0c326593d3a77c1062081d66d7417770586fceb7
Alex Klubovich <avklubovich@gmail.com>
2017-01-06 12:09:51 +0300

    small fix gitignore and config.php

--- web/config.php
+++ web/config.php
@@ -3,14 +3,8 @@
 class Config
 {
     const TARGET_DIR_FILES = "files/";
-    
-    const HOST_DB = "192.168.202.214";
-    const PORT_DB = 5432;
-    const NAME_DB = "auto_review_db";
-    const USER_DB = "postgres";
-    const PASS_DB = "11";
 
-    const HOST_CPP = "http://192.168.202.214:81/";
+    const HOST_CPP = "http://192.168.202.176:81/";
 }
 
 ?>
