155e01259f80a3980d8ef322ba24cb78a988052a
Alex Klubovich <avklubovich@gmail.com>
2017-01-11 17:02:31 +0300

    fix

--- src/Commands/ChangeCarStatus.cpp
+++ src/Commands/ChangeCarStatus.cpp
@@ -32,10 +32,10 @@ network::ResponseShp ChangeCarStatus::exec()
     const auto& userPass = bodyData["password"].toString();
 
     auto webManager = network::WebRequestManager::instance();
-    auto webRequest = network::WebRequestShp::create("type_query");
+    auto webRequest = network::WebRequestShp::create("sub_qry");
 
     QVariantMap userData;
-    userData["type_query"] = "autoreview_change_auto_status";
+    userData["sub_qry"] = "autoreview_change_auto_status";
     userData["user_login"] = userLogin;
     userData["user_pass"] = QString(QCryptographicHash::hash(userPass.toStdString().data(), QCryptographicHash::Md5).toHex());
     userData["auto_id"] = QString::number(autoId);
