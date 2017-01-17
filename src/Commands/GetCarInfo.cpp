01706546a7747ba6431c0497608e07666173642c
Alex Klubovich <avklubovich@gmail.com>
2017-01-17 13:44:18 +0300

    edit GetCarInfo

--- src/Commands/GetCarInfo.cpp
+++ src/Commands/GetCarInfo.cpp
@@ -29,21 +29,18 @@ network::ResponseShp GetCarInfo::exec()
     const auto& incomingData = _context._packet.body().toMap();
     const auto& bodyData = incomingData.value("body").toMap();
 
-//    if (!bodyData.contains("login") ||
-//        !bodyData.contains("password") ||
-//        !bodyData.contains("id_car"))
-//    {
-//        sendError("Do not send field", "field_error", signature());
-//        return network::ResponseShp();
-//    }
+    if (!bodyData.contains("login") ||
+        !bodyData.contains("password") ||
+        !bodyData.contains("id_car"))
+    {
+        sendError("Do not send field", "field_error", signature());
+        return network::ResponseShp();
+    }
 
     const auto carId = bodyData["id_car"].toInt();
 
-//    const auto& userLogin = bodyData["login"].toString();
-//    const auto& userPass = bodyData["password"].toString();
-
-    const QString& userLogin = "test_andrej";
-    const QString& userPass = "andrttest";
+    const auto& userLogin = bodyData["login"].toString();
+    const auto& userPass = bodyData["password"].toString();
 
     auto webManager = network::WebRequestManager::instance();
     auto webRequest = network::WebRequestShp::create("type_query");
