1e37866d5e6087dedeb02de845794a6bd5fdceca
Alex Klubovich <avklubovich@gmail.com>
2017-02-13 15:46:25 +0300

    add loading data from remote server

--- src/Commands/WSGetStartData.cpp
+++ src/Commands/WSGetStartData.cpp
@@ -8,6 +8,9 @@
 #include "database/DBWraper.h"
 #include "database/DBHelpers.h"
 
+#include "web-exchange/WebRequestManager.h"
+#include "web-exchange/WebRequest.h"
+
 RegisterCommand(auto_review::WSGetStartData, "ws_get_start_data")
 
 
@@ -87,6 +90,55 @@ network::ResponseShp WSGetStartData::exec()
     }
 
     const auto& damages = database::DBHelpers::queryToVariant(selectDamagesQuery);
+
+    const auto& incomingData = _context._packet.body().toMap();
+    const auto& bodyData = incomingData.value("body").toMap();
+    const auto& userLogin = bodyData["login"].toString();
+    const auto& userPass = bodyData["password"].toString();
+
+    auto webManager = network::WebRequestManager::instance();
+    auto webRequest = network::WebRequestShp::create("type_query");
+
+    QVariantMap userData;
+    userData["type_query"] = "get_autos_data";
+    userData["auto_id"] = carIdsList.join(",");
+    userData["user_login"] = userLogin;
+    userData["user_pass"] = QString(QCryptographicHash::hash(userPass.toStdString().data(), QCryptographicHash::Md5).toHex());
+    webRequest->setArguments(userData);
+    webRequest->setCallback(nullptr);
+
+    webManager->sendRequestCurrentThread(webRequest);
+
+    const auto& data = webRequest->reply();
+    webRequest->release();
+
+    const auto& doc = QJsonDocument::fromJson(data);
+    const auto& jobj = doc.object();
+    const auto& map = jobj.toVariantMap();
+
+    if (!map.contains("status"))
+    {
+        sendError("Bad response from remote server", "remove_server_error", signature());
+        qDebug() << __FUNCTION__ << "error: field not sended";
+        return network::ResponseShp();
+    }
+
+    const auto status = map["status"].toInt();
+    if (status != 1)
+    {
+        const auto& errorList = map["error"].toList();
+        const auto& errorStr = errorList.first().toString();
+        sendError(errorStr, "remove_server_error", signature());
+        return network::ResponseShp();
+    }
+
+    const auto& array = map["array"].toList();
+    if (array.isEmpty())
+    {
+        sendError("Bad response from remote server", "remove_server_error", signature());
+        qDebug() << __FUNCTION__ << "error: array is empty";
+        return network::ResponseShp();
+    }
 
     QMap<quint64, QVariantMap> carsInfoMap;
     for (auto& carInfo : carsList)
